8461992ad1b40e97a6757e08ec0d46736706ebe3
Stefan Starosta <stefan.starosta@gmail.com>
2023-03-28 10:33:12 +0200

    added dummy controller for listing servers

---
+++ src/main/java/com/chookity/servermanagerbackendapplication/resource/ServerResource.java
@@ -0,0 +1,34 @@
+package com.chookity.servermanagerbackendapplication.resource;
+
+import com.chookity.servermanagerbackendapplication.model.Response;
+import com.chookity.servermanagerbackendapplication.service.impl.ServerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+    //TODO tight coupling
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+}
