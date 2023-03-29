9a57f586d6ba5bdcbbbcb39da808aa9af33d1732
Stefan Starosta <stefan.starosta@gmail.com>
2023-03-29 09:26:14 +0200

    finished controller and added configuration for db

--- src/main/java/com/chookity/servermanagerbackendapplication/ServerManagerBackendApplication.java
+++ src/main/java/com/chookity/servermanagerbackendapplication/ServerManagerBackendApplication.java
@@ -1,7 +1,12 @@
 package com.chookity.servermanagerbackendapplication;
 
+import com.chookity.servermanagerbackendapplication.enumeration.Status;
+import com.chookity.servermanagerbackendapplication.model.Server;
+import com.chookity.servermanagerbackendapplication.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerManagerBackendApplication {
@@ -10,4 +15,13 @@ public class ServerManagerBackendApplication {
 		SpringApplication.run(ServerManagerBackendApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(final ServerRepository serverRepository){
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.30", "Fedora Linux", "60.3GB", "Mobile Phone", "http://localhost:8080/server/image/server2.png", Status.SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.42", "MS DOS", "512GB", "Dell Tower", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.14", "OS2", "64GB", "HP BladeSystem", "http://localhost:8080/server/image/server4.png", Status.SERVER_DOWN));
+		};
+	}
 }
--- src/main/java/com/chookity/servermanagerbackendapplication/resource/ServerResource.java
+++ src/main/java/com/chookity/servermanagerbackendapplication/resource/ServerResource.java
@@ -1,14 +1,19 @@
 package com.chookity.servermanagerbackendapplication.resource;
 
+import com.chookity.servermanagerbackendapplication.enumeration.Status;
 import com.chookity.servermanagerbackendapplication.model.Response;
+import com.chookity.servermanagerbackendapplication.model.Server;
 import com.chookity.servermanagerbackendapplication.service.impl.ServerServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -30,5 +35,64 @@ public class ServerResource {
                         .statusCode(HttpStatus.OK.value())
                         .build()
         );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") final String ipAddress) throws IOException {
+        final Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", server))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> pingServer(@RequestBody @Valid final Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .message("Server created")
+                        .status(HttpStatus.CREATED)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") final Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.get(id)))
+                        .message("Server retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") final Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("deleted", serverService.delete(id)))
+                        .message("Server deleted")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping(path = "/image/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") final String fileName) throws IOException {
+        //TODO change to relativePath
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName));
     }
 }
